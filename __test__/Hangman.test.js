a3d6fc4994189fdb374b59beb52309c6890d91fe
Bea Sanssi <bs222eh@student.lnu.se>
2024-11-26 15:25:28 +0100

    Add authors

--- __test__/Hangman.test.js
+++ __test__/Hangman.test.js
@@ -1,3 +1,11 @@
+/**
+ * This is a test file for the Auth check component.
+ * @test {Hangman}
+ * @version 1.0.0
+ * @author Beatriz Sanssi <bs222eh@student.lnu.se>
+ * @author Sabrina Prichard-Lybeck <sp223kz@student.lnu.se> 
+ */
+
 describe('Hangman', () => {
   it('should render Hangman component', () => {
     const hangman = new Hangman()
