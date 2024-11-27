962bc808ce67410285f8d3eb7daf90b57e57f6db
Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
2024-11-27 12:55:08 +0100

    GREEN: Hangman component successfully instantiated

--- __test__/Hangman.test.js
+++ __test__/Hangman.test.js
@@ -1,16 +1,16 @@
 /**
- * This is a test file for the Auth check component.
+ * This is a test file for the Hangman Game component.
  * @test {Hangman}
  * @version 1.0.0
  * @author Beatriz Sanssi <bs222eh@student.lnu.se>
  * @author Sabrina Prichard-Lybeck <sp223kz@student.lnu.se> 
  */
+import { Hangman } from '../src/components/Hangman.js'
 
 describe('Hangman', () => {
   it('should render Hangman component', () => {
     const hangman = new Hangman()
-    // const Hangman = require('../Hangman')
-    expect(Hangman).toBeDefined()
+    expect(hangman).toBeDefined()
   })
   // it('should throw an error if there are no words in wordlist', () => {
   //   const hangman = new Hangman()
---
+++ src/components/Hangman.js
@@ -0,0 +1,8 @@
+/** 
+ * Class that represents the Hangman Game orchestrator. 
+ */
+export class Hangman {
+  constructor() {
+    console.log("Hangman instance created")
+  }
+}
