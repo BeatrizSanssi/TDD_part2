5dc5d0c581aba9ce7aeefb3128d8142dfce46cae
Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
2024-12-02 13:18:59 +0100

    TEST COVERAGE: Ran into test coverage issue, working on solving it before we continue with the coding

--- src/components/HangmanOrchestrator.js
+++ src/components/HangmanOrchestrator.js
@@ -4,8 +4,15 @@
  * @author Beatriz Sanssi <bs222eh@student.lnu.se>
  * @author Sabrina Prichard-Lybeck <sp223kz@student.lnu.se> 
  */
+import { WordList } from './WordList.js'
+import { WordGuesser } from './WordGuesser.js'
 export class HangmanOrchestrator {
+  #wordList
+  #wordGuesser
+
   constructor() {
+    this.#wordList = new WordList()
+    this.#wordGuesser = new WordGuesser()
   }
 }
 
--- src/components/WordGuesser.js
+++ src/components/WordGuesser.js
@@ -1,6 +1,10 @@
+/**
+ * Class that represents a word guesser.
+ * @version 1.0.0
+ * @author Beatriz Sanssi <bs222eh@student.lnu.se>
+ * @author Sabrina Prichard-Lybeck <sp223kz@student.lnu.se> 
+ */
 export class WordGuesser {
-
-
   checkGuess(letter, word) {
     return word.includes(letter)
   }
