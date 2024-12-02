c57a274ace81949ba504fabd3096c84936ca3d97
Bea Sanssi <bs222eh@student.lnu.se>
2024-12-02 14:04:37 +0100

    Generated basic html and added basic styling

---
+++ index.js
@@ -0,0 +1,12 @@
+import { HangmanOrchestrator } from './src/components/HangmanOrchestrator.js'
+
+const orchestrator = new HangmanOrchestrator()
+const wordList = orchestrator.getWordList()
+const wordGuesser = orchestrator.getWordGuesser()
+
+let selectedWord = wordList.selectRandomWord()
+
+const wordPlaceholder = document.getElementById('word-placeholder')
+const guessInput = document.getElementById('guess-input')
+const submitGuess = document.getElementById('submit-guess')
+const message = document.getElementById('message')
