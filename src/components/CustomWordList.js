b49cce579faa1888e28a70c4904c1062acfedca0
Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
2024-12-03 11:48:45 +0100

    GREEN: unit test that checks that an instance of CustomWordList can be created now passes

---
+++ src/components/CustomWordList.js
@@ -0,0 +1,9 @@
+/**
+ * Class that represents a customized word list.
+ * @version 1.0.0
+ * @author Beatriz Sanssi <bs222eh@student.lnu.se>
+ * @author Sabrina Prichard-Lybeck <sp223kz@student.lnu.se> 
+ */
+export class CustomWordList {
+  #wordList = []
+}
