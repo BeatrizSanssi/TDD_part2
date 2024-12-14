6970c8fab69a3412aef54c515ed3cc718e612028
Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
2024-12-14 11:39:15 +0100

    GREEN: test now passes since Alphabet class has been created

--- __test__/Alphabet.test.js
+++ __test__/Alphabet.test.js
@@ -1,7 +1,7 @@
-import { Alphabet } from '../src/Alphabet'
+import { Alphabet } from '../src/components/Alphabet.js'
 
 describe('Alphabet', () => {
-  it('should create an instance of the alphabet', () => {
+  it('should create an instance of the Alphabet class', () => {
     const alphabet = new Alphabet()
     expect(alphabet).toBeDefined()
   })
---
+++ src/components/Alphabet.js
@@ -0,0 +1,9 @@
+/**
+ * Class that represents the alphabet.
+ * @version 1.0.0
+ * @author Beatriz Sanssi <bs222eh@student.lnu.se>
+ * @author Sabrina Prichard-Lybeck <sp223kz@student.lnu.se> 
+ */
+export class Alphabet {
+  #alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+}
