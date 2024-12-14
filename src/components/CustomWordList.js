8a1e3585c6b3dac643afbeac08bdb9646a2bba72
Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
2024-12-14 11:26:30 +0100

    REFACTOR: updated disabledGuessedLetter method so that test now passes correct implementation

--- src/components/CustomWordList.js
+++ src/components/CustomWordList.js
@@ -55,8 +55,9 @@ export class CustomWordList extends WordList {
    * @param {String} word
    */
   disableGuessedLetter(guessedLetter) {
-    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    const updatedAlphabet = alphabet.filter(letter => letter !== guessedLetter)
 
-    return alphabet
+    return updatedAlphabet
   }
 }
