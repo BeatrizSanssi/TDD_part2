aee48bb8f782b737bc620d774b8e1d75a7f1753b
Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
2024-12-14 11:33:57 +0100

    RED: failed unit test that checks if an instance of the not yet implemented class Alphabet can be created

---
+++ __test__/Alphabet.test.js
@@ -0,0 +1,8 @@
+import { Alphabet } from '../src/Alphabet'
+
+describe('Alphabet', () => {
+  it('should create an instance of the alphabet', () => {
+    const alphabet = new Alphabet()
+    expect(alphabet).toBeDefined()
+  })
+})
